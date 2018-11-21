7c7ac6bcc4b681cf37c16683a7f4215ab4ffe5be
ipek <ipekcivicakan@gmail.com>
2018-11-21 09:37:50 -0800

    Getting users physical location and display on the screen

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+  constructor(props){
+    super(props);
+
+    this.state = {lat: null};
+
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({lat: position.coords.latitude})
+      },
+      err => console.log(err)
     );
+
+  }
+
+  render(){
+    return(
+      <div>Latitude: {this.state.lat} </div>
+    )
   }
 }
-
 export default App;
