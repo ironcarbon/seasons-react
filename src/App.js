9127a16b7295cb2bd1e8c3d146344ebab2e88eea
ipek <ipekcivicakan@gmail.com>
2018-11-21 12:24:08 -0800

    Conditionally rendering content

--- src/App.js
+++ src/App.js
@@ -4,21 +4,33 @@ class App extends Component {
   constructor(props){
     super(props);
 
-    this.state = {lat: null};
+    this.state = {lat: null, errorMessage: ''};
 
     navigator.geolocation.getCurrentPosition(
       position => {
         this.setState({lat: position.coords.latitude})
       },
-      err => console.log(err)
+      err => {
+        //console.log(err)
+        this.setState({errorMessage: err.message})
+      }
     );
 
   }
 
   render(){
-    return(
-      <div>Latitude: {this.state.lat} </div>
-    )
+  if(this.state.errorMessage && !this.state.lat){
+    return <div>Error:{this.state.errorMessage}</div>
+  } 
+
+  if(this.state.lat && !this.state.errorMessage){
+    return <div>Latitude: {this.state.lat}</div>
+  } 
+
+  return <div>Loading!</div>
+     
+      
+    
   }
 }
 export default App;
